08bf50c0084e25f16f74a0da4e220806f6eadb66
アレクサンダー.eth <gpg@pavlovcik.com>
2023-11-27 23:01:27 +0900

    refactor: move index

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as github from "@actions/github";
 import { createClient } from "@supabase/supabase-js";
 import OpenAI from "openai";
 import zlib from "zlib";
-import { issueClosed } from "./src/handlers/issue/issue-closed";
+import { issueClosed } from "./handlers/issue/issue-closed";
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 if (!OPENAI_API_KEY) {
@@ -23,23 +23,22 @@ if (!SUPABASE_KEY) {
 // Define an asynchronous function to handle the logic
 async function run() {
   try {
-    const payload = JSON.parse(
-      JSON.stringify(github.context.payload, null, 2)
-    );
+    const payload = JSON.parse(JSON.stringify(github.context.payload, null, 2));
     const eventName = payload.inputs.eventName;
     const handlerPayload = JSON.parse(payload.inputs.payload);
 
     if (eventName === "issueClosed") {
-      const SUPABASE_CLIENT = createClient(
-        SUPABASE_URL,
-        SUPABASE_KEY
-      );
-      const result: string = await issueClosed(
-        { issue: handlerPayload.issue, issueComments: handlerPayload.issueComments, openAi: new OpenAI({ apiKey: process.env.OPENAI_API_KEY }), repoCollaborators: handlerPayload.repoCollaborators, pullRequestComments: handlerPayload.pullRequestComments, config: handlerPayload.botConfig, supabase: SUPABASE_CLIENT },
-      );
-      const compressedString = zlib.gzipSync(
-        Buffer.from(result.replace(/<!--[\s\S]*?-->/g, ""))
-      );
+      const SUPABASE_CLIENT = createClient(SUPABASE_URL, SUPABASE_KEY);
+      const result: string = await issueClosed({
+        issue: handlerPayload.issue,
+        issueComments: handlerPayload.issueComments,
+        openAi: new OpenAI({ apiKey: process.env.OPENAI_API_KEY }),
+        repoCollaborators: handlerPayload.repoCollaborators,
+        pullRequestComments: handlerPayload.pullRequestComments,
+        config: handlerPayload.botConfig,
+        supabase: SUPABASE_CLIENT,
+      });
+      const compressedString = zlib.gzipSync(Buffer.from(result.replace(/<!--[\s\S]*?-->/g, "")));
       core.setOutput("result", {
         comment: compressedString.toJSON(),
       });
