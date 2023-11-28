96f13d6426775184c53ef8847c78de6f31e2cc57
アレクサンダー.eth <gpg@pavlovcik.com>
2023-11-28 14:17:38 +0900

    refactor: cleanup

--- src/check-env.ts
+++ src/check-env.ts
@@ -1,19 +1,17 @@
-import * as core from "@actions/core";
-
 export function checkEnvironmentVariables() {
   const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
   if (!OPENAI_API_KEY) {
-    core.setFailed("OPENAI_API_KEY not set");
+    throw new Error("OPENAI_API_KEY not set");
   }
 
   const SUPABASE_URL = process.env.SUPABASE_URL;
 
   if (!SUPABASE_URL) {
-    core.setFailed("SUPABASE_URL not set");
+    throw new Error("SUPABASE_URL not set");
   }
   const SUPABASE_KEY = process.env.SUPABASE_KEY;
   if (!SUPABASE_KEY) {
-    core.setFailed("SUPABASE_KEY not set");
+    throw new Error("SUPABASE_KEY not set");
   }
   return { SUPABASE_URL, SUPABASE_KEY };
 }
--- src/index.ts
+++ src/index.ts
@@ -5,34 +5,40 @@ import OpenAI from "openai";
 import zlib from "zlib";
 import { checkEnvironmentVariables } from "./check-env";
 import { issueClosed } from "./handlers/issue/issue-closed";
-
-async function run() {
-  const { SUPABASE_URL, SUPABASE_KEY } = checkEnvironmentVariables();
-  const payload = JSON.parse(JSON.stringify(github.context.payload, null, 2));
-  const eventName = payload.inputs.eventName;
-  const handlerPayload = JSON.parse(payload.inputs.payload);
-
-  if (eventName === "issueClosed") {
-    const SUPABASE_CLIENT = createClient(SUPABASE_URL, SUPABASE_KEY);
-    const result: string = await issueClosed({
-      issue: handlerPayload.issue,
-      issueComments: handlerPayload.issueComments,
-      openAi: new OpenAI({ apiKey: process.env.OPENAI_API_KEY }),
-      repoCollaborators: handlerPayload.repoCollaborators,
-      pullRequestComments: handlerPayload.pullRequestComments,
-      config: handlerPayload.botConfig,
-      supabase: SUPABASE_CLIENT,
-    });
-    const compressedString = zlib.gzipSync(Buffer.from(result.replace(/<!--[\s\S]*?-->/g, "")));
-    core.setOutput("result", {
-      comment: compressedString.toJSON(),
-    });
-  }
-}
+// import cloneDeep from "lodash/cloneDeep";
 
 run()
-  // .then(() => {})
+  .then((result) => core.setOutput("result", result))
   .catch((error) => {
     console.error(error);
     core.setFailed(error);
   });
+
+async function run() {
+  const { SUPABASE_URL, SUPABASE_KEY } = checkEnvironmentVariables();
+  // const payload = cloneDeep(github.context.payload);
+  const webhookPayload = github.context.payload;
+
+  const eventName = webhookPayload.inputs.eventName;
+  const handlerPayload = JSON.parse(webhookPayload.inputs.payload);
+
+  if (eventName === "issueClosed") {
+    return await issueClosedEventHandler(SUPABASE_URL, SUPABASE_KEY, handlerPayload);
+  }
+}
+
+async function issueClosedEventHandler(supabaseUrl: string, supabaseKey: string, payload: any) {
+  const SUPABASE_CLIENT = createClient(supabaseUrl, supabaseKey);
+  const result: string = await issueClosed({
+    issue: payload.issue,
+    issueComments: payload.issueComments,
+    openAi: new OpenAI({ apiKey: process.env.OPENAI_API_KEY }),
+    repoCollaborators: payload.repoCollaborators,
+    pullRequestComments: payload.pullRequestComments,
+    config: payload.botConfig,
+    supabase: SUPABASE_CLIENT,
+  });
+  const compressedString = zlib.gzipSync(Buffer.from(result.replace(/<!--[\s\S]*?-->/g, "")));
+
+  return compressedString.toJSON();
+}
