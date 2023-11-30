f5e99579b8f888319cfb44b91b10cdeff388d569
danddy <danddycandy@gmail.com>
2023-11-30 08:22:43 -0500

    feat: minor fix

--- src/handlers/issue/generate-permits.ts
+++ src/handlers/issue/generate-permits.ts
@@ -42,10 +42,7 @@ async function generateComment(totals: TotalsById, issue: Issue, config: BotConf
 
     if (!evmPrivateEncrypted) throw console.warn("No bot wallet private key defined");
 
-    const { data, error } = await supabase
-      .from("users")
-      .select("*, wallets(*)")
-      .filter("id", "eq", parseInt(userId));
+    const { data, error } = await supabase.from("users").select("*, wallets(*)").filter("id", "eq", parseInt(userId));
     if (error) throw error;
 
     const beneficiaryAddress = data.length > 0 ? data[0] : "";
