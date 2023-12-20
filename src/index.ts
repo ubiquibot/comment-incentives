e5b6608b170778f8426266128e4e850441a514a9
アレクサンダー.eth <gpg@pavlovcik.com>
2023-12-21 08:36:14 +0900

    Squashed commit of the following:
    commit 872de35ffd9eaf629212ffa21705a6d2c198357a
    Author: danddy <danddycandy@gmail.com>
    Date:   Mon Dec 18 13:47:52 2023 -0500
    
        feat: evm update
    
    commit 83e4e13a8e80cf62ae741f25b3c508cd2fd2d0f8
    Author: danddy <danddycandy@gmail.com>
    Date:   Mon Dec 18 13:41:29 2023 -0500
    
        feat: update
    
    commit 8a7854eb0a7823765271dc4e00005b20eb486029
    Author: danddy <danddycandy@gmail.com>
    Date:   Mon Dec 18 10:39:25 2023 -0500
    
        feat: update
    
    commit ab35f7673c15d839f574493ab4c642328a5f8844
    Author: danddy <danddycandy@gmail.com>
    Date:   Mon Dec 18 10:33:28 2023 -0500
    
        feat: update
    
    commit bc4930adca21fc02f885ec012bb9e426a79d2dd1
    Author: danddy <danddycandy@gmail.com>
    Date:   Mon Dec 18 10:25:18 2023 -0500
    
        feat: update
    
    commit 834558fa4a9c0636694282a8afd3b5634186f8a6
    Author: danddy <danddycandy@gmail.com>
    Date:   Mon Dec 18 10:24:44 2023 -0500
    
        feat: update
    
    commit 11f10aedcf249ae939a94a6a3cd4a5ab8cd0c406
    Author: danddy <danddycandy@gmail.com>
    Date:   Mon Dec 18 10:23:39 2023 -0500
    
        feat: update
    
    commit fa4e322cddb364531dbac0b42ea1f6f3e6410211
    Author: danddy <danddycandy@gmail.com>
    Date:   Mon Dec 18 10:14:39 2023 -0500
    
        feat: update
    
    commit 857ad82954984daaab81b3101201769864b60ee4
    Author: danddy <danddycandy@gmail.com>
    Date:   Mon Dec 18 10:12:38 2023 -0500
    
        feat: update
    
    commit 6bfa5cc0e932fa92e7216f179e7d369e6ced06e3
    Author: danddy <danddycandy@gmail.com>
    Date:   Mon Dec 18 10:07:05 2023 -0500
    
        feat: update
    
    commit aae4a399ac48414e34117cf3190ed75988d13458
    Author: danddy <danddycandy@gmail.com>
    Date:   Mon Dec 18 09:12:52 2023 -0500
    
        feat: update
    
    commit 9e50cc941a41e41ae0323c1ea4bad08f8e86d3c1
    Author: danddy <danddycandy@gmail.com>
    Date:   Wed Dec 13 05:55:32 2023 -0500
    
        feat: pass less info
    
    commit 5318d4663bccf187ee6eaf540d0262186d90f322
    Merge: 994e5e5 48beac1
    Author: danddy <danddycandy@gmail.com>
    Date:   Tue Dec 12 14:11:22 2023 -0500
    
        Merge branch 'feat/pass-less-info-in' of https://github.com/pavlovcik/ubiquibot-config into feat/pass-less-info
    
    commit 994e5e563a98bab56e5ae5f744782414938649cf
    Merge: 8c42ee2 d9cd524
    Author: FUTURE <danddycandy@gmail.com>
    Date:   Tue Dec 12 11:07:22 2023 -0800
    
        Merge branch 'pavlovcik:development' into development
    
    commit 8c42ee263785949444c4bd69235e098c41217518
    Author: FUTURE <danddycandy@gmail.com>
    Date:   Tue Dec 5 08:06:14 2023 -0500
    
        Update ubiquibot-config.yml

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import { Octokit } from "@octokit/rest";
 import { SupabaseClient, createClient } from "@supabase/supabase-js";
+import OpenAI from "openai";
 import zlib from "zlib";
 import { checkEnvironmentVariables } from "./check-env";
 import { issueClosed } from "./handlers/issue/issue-closed";
-import { GitHubEvent } from "./types/payload";
+import { getLinkedPullRequests } from "./helpers/get-linked-issues-and-pull-requests";
+import { BotConfig, GitHubComment, GitHubEvent, GitHubIssue } from "./types/payload";
+import { generateConfiguration } from "./utils/generate-configuration";
+
+export const octokit: Octokit = new Octokit({});
 
 run()
   .then((result) => core.setOutput("result", result))
@@ -15,16 +21,18 @@ run()
 
 interface DelegatedComputeInputs {
   eventName: GitHubEvent;
+  organization: string;
   issueOwner: string;
   issueRepository: string;
-  issueNumber: number;
+  issueNumber: string;
+  repoCollaborators: string;
 }
 
 async function run() {
   const { SUPABASE_URL, SUPABASE_KEY } = checkEnvironmentVariables();
   const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
   const webhookPayload = github.context.payload;
-  const inputs = JSON.parse(webhookPayload.inputs) as DelegatedComputeInputs;
+  const inputs = webhookPayload.inputs as DelegatedComputeInputs;
   const eventName = inputs.eventName;
   if (GitHubEvent.ISSUES_CLOSED === eventName) {
     return await issueClosedEventHandler(supabaseClient, inputs);
@@ -34,19 +42,19 @@ async function run() {
 }
 
 async function issueClosedEventHandler(supabaseClient: SupabaseClient, inputs: DelegatedComputeInputs) {
-  const issue = await getIssue(inputs.issueOwner, inputs.issueRepository, inputs.issueNumber);
-  const issueComments = await getIssueComments(issue);
-  const pullRequestComments = await getPullRequestComments(issue);
-  const repoCollaborators = await getRepoCollaborators(issue);
+  const issueNumber = Number(inputs.issueNumber);
+  const issue = await getIssue(inputs.issueOwner, inputs.issueRepository, issueNumber);
+  const issueComments = await getIssueComments(inputs.issueOwner, inputs.issueRepository, issueNumber);
+  const pullRequestComments = await getPullRequestComments(inputs.issueOwner, inputs.issueRepository, issueNumber);
 
   const openAi = getOpenAi();
-  const config = await getConfig(inputs.issueOwner, inputs.issueRepository);
+  const config = await getConfig(inputs.organization, inputs.issueOwner, inputs.issueRepository);
 
   const result: string = await issueClosed({
     issue,
     issueComments,
     pullRequestComments,
-    repoCollaborators,
+    repoCollaborators: JSON.parse(inputs.repoCollaborators),
     openAi,
     config,
     supabase: supabaseClient,
@@ -57,17 +65,63 @@ async function issueClosedEventHandler(supabaseClient: SupabaseClient, inputs: D
 }
 
 // TODO: finish implementing these functions
-// async function getIssue(owner: string, repository: string, issueNumber: number): Promise<GitHubIssue> {}
-// async function getIssueComments(issue: GitHubIssue): Promise<GitHubComment[]> {}
-// async function getPullRequestComments(issue: GitHubIssue): Promise<GitHubComment[]> {}
-// async function getRepoCollaborators(issue: GitHubIssue): Promise<GitHubUser[]> {}
-// function getOpenAi(): OpenAI {
-//   if (!process.env.OPENAI_API_KEY) {
-//     throw new Error("OPENAI_API_KEY is not set");
-//   }
-//   return new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-// }
-// async function getConfig(owner: string, repository: string): Promise<BotConfig> {
-//   // TODO: fetch config from source repository.
-//   // Ensure that its organization config is also fetched and merged properly as we do normally during bot startup.
-// }
+async function getIssue(owner: string, repository: string, issueNumber: number): Promise<GitHubIssue> {
+  try {
+    const { data: issue } = await octokit.rest.issues.get({
+      owner: owner,
+      repo: repository,
+      issue_number: issueNumber,
+    });
+    return issue as GitHubIssue;
+  } catch (e: unknown) {
+    throw new Error("fetching issue failed!");
+  }
+}
+
+async function getIssueComments(
+  owner: string,
+  repository: string,
+  issueNumber: number,
+  format: "raw" | "html" | "text" | "full" = "raw"
+): Promise<GitHubComment[]> {
+  try {
+    const comments = (await octokit.paginate(octokit.rest.issues.listComments, {
+      owner,
+      repo: repository,
+      issue_number: issueNumber,
+      per_page: 100,
+      mediaType: {
+        format,
+      },
+    })) as GitHubComment[];
+    return comments;
+  } catch (e: unknown) {
+    throw new Error("Fetching all issue comments failed!");
+  }
+}
+async function getPullRequestComments(
+  owner: string,
+  repository: string,
+  issueNumber: number
+): Promise<GitHubComment[]> {
+  const pullRequestComments: GitHubComment[] = [];
+  const linkedPullRequests = await getLinkedPullRequests({ owner, repository, issue: issueNumber });
+  if (linkedPullRequests.length) {
+    const linkedCommentsPromises = linkedPullRequests.map((pull) => getIssueComments(owner, repository, pull.number));
+    const linkedCommentsResolved = await Promise.all(linkedCommentsPromises);
+    for (const linkedComments of linkedCommentsResolved) {
+      pullRequestComments.push(...linkedComments);
+    }
+  }
+  return pullRequestComments;
+}
+
+function getOpenAi(): OpenAI {
+  if (!process.env.OPENAI_API_KEY) {
+    throw new Error("OPENAI_API_KEY is not set");
+  }
+  return new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+}
+async function getConfig(organization: string, owner: string, repository: string): Promise<BotConfig> {
+  return generateConfiguration(organization, owner, repository);
+}
---
+++ src/utils/generate-configuration.ts
@@ -0,0 +1,139 @@
+import { Value } from "@sinclair/typebox/value";
+import { DefinedError } from "ajv";
+import mergeWith from "lodash/merge";
+import YAML from "yaml";
+import { BotConfig, stringDuration, validateBotConfig } from "../types/configuration-types";
+import { octokit } from "..";
+
+const UBIQUIBOT_CONFIG_REPOSITORY = "ubiquibot-config";
+const UBIQUIBOT_CONFIG_FULL_PATH = ".github/ubiquibot-config.yml";
+
+export async function generateConfiguration(organization: string, owner: string, repository: string): Promise<BotConfig> {
+
+  const orgConfig = parseYaml(
+    await download({
+      repository: UBIQUIBOT_CONFIG_REPOSITORY,
+      owner: organization || owner,
+    })
+  );
+
+  const repoConfig = parseYaml(
+    await download({
+      repository: repository,
+      owner,
+    })
+  );
+
+  const merged = mergeWith({}, orgConfig, repoConfig, (objValue: unknown, srcValue: unknown) => {
+    if (Array.isArray(objValue) && Array.isArray(srcValue)) {
+      // if it's string array, concat and remove duplicates
+      if (objValue.every((value) => typeof value === "string")) {
+        return [...new Set(objValue.concat(srcValue))];
+      }
+      // otherwise just concat
+      return objValue.concat(srcValue);
+    }
+  });
+
+  const isValid = validateBotConfig(merged);
+  if (!isValid) {
+    const errorMessage = getErrorMsg(validateBotConfig.errors as DefinedError[]);
+    if (errorMessage) {
+      throw new Error("Invalid merged configuration");
+    }
+  }
+
+  // this will run transform functions
+  try {
+    transformConfig(merged);
+  } catch (err) {
+      throw new Error("Configuration error");
+  }
+
+  // console.dir(merged, { depth: null, colors: true });
+  return merged as BotConfig;
+}
+
+// Transforming the config only works with Typebox and not Ajv
+// When you use Decode() it not only transforms the values but also validates the whole config and Typebox doesn't return all errors so we can filter for correct ones
+// That's why we have transform every field manually and catch errors
+export function transformConfig(config: BotConfig) {
+  let errorMsg = "";
+  try {
+    config.timers.reviewDelayTolerance = Value.Decode(stringDuration(), config.timers.reviewDelayTolerance);
+  } catch (err: unknown) {
+    const decodeError = err as DecodeError;
+    if (decodeError.value) {
+      errorMsg += `Invalid reviewDelayTolerance value: ${decodeError.value}\n`;
+    }
+  }
+  try {
+    config.timers.taskStaleTimeoutDuration = Value.Decode(stringDuration(), config.timers.taskStaleTimeoutDuration);
+  } catch (err: unknown) {
+    const decodeError = err as DecodeError;
+    if (decodeError.value) {
+      errorMsg += `Invalid taskStaleTimeoutDuration value: ${decodeError.value}\n`;
+    }
+  }
+  try {
+    config.timers.taskFollowUpDuration = Value.Decode(stringDuration(), config.timers.taskFollowUpDuration);
+  } catch (err: unknown) {
+    const decodeError = err as DecodeError;
+    if (decodeError.value) {
+      errorMsg += `Invalid taskFollowUpDuration value: ${decodeError.value}\n`;
+    }
+  }
+  try {
+    config.timers.taskDisqualifyDuration = Value.Decode(stringDuration(), config.timers.taskDisqualifyDuration);
+  } catch (err: unknown) {
+    const decodeError = err as DecodeError;
+    if (decodeError.value) {
+      errorMsg += `Invalid taskDisqualifyDuration value: ${decodeError.value}\n`;
+    }
+  }
+  if (errorMsg) throw new Error(errorMsg);
+}
+
+function getErrorMsg(errors: DefinedError[]) {
+  const errorsWithoutStrict = errors.filter((error) => error.keyword !== "additionalProperties");
+  return errorsWithoutStrict.length === 0
+    ? null
+    : errorsWithoutStrict.map((error) => error.instancePath.replaceAll("/", ".") + " " + error.message).join("\n");
+}
+
+async function download({
+  repository,
+  owner,
+}: {
+  repository: string;
+  owner: string;
+}): Promise<string | null> {
+  if (!repository || !owner) throw new Error("Repo or owner is not defined");
+  try {
+    const { data } = await octokit.rest.repos.getContent({
+      owner,
+      repo: repository,
+      path: UBIQUIBOT_CONFIG_FULL_PATH,
+      mediaType: { format: "raw" },
+    });
+    return data as unknown as string; // this will be a string if media format is raw
+  } catch (err) {
+    return null;
+  }
+}
+
+export function parseYaml(data: null | string) {
+  try {
+    if (data) {
+      const parsedData = YAML.parse(data);
+      return parsedData ?? null;
+    }
+  } catch (error) {
+    throw new Error("Failed to parse YAML");
+  }
+  return null;
+}
+
+interface DecodeError extends Error {
+  value?: string;
+}
