6a13c305536e6788b68c52b834495ce85273290d
アレクサンダー.eth <gpg@pavlovcik.com>
2023-12-24 00:45:37 +0900

    fix: parse array

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 import { Octokit } from "@octokit/rest";
 import { SupabaseClient, createClient } from "@supabase/supabase-js";
+import OpenAI from "openai";
 import { checkEnvironmentVariables } from "./check-env";
 import { issueClosed } from "./handlers/issue/issue-closed";
 import { getLinkedPullRequests } from "./helpers/get-linked-issues-and-pull-requests";
 import { BotConfig, GitHubComment, GitHubEvent, GitHubIssue, GitHubUser } from "./types/payload";
 import { generateConfiguration } from "./utils/generate-configuration";
-import OpenAI from "openai";
 
 export const octokit: Octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
@@ -23,14 +23,22 @@ interface DelegatedComputeInputs {
   issueOwner: string;
   issueRepository: string;
   issueNumber: string;
-  collaborators: string[];
+  collaborators: string;
 }
+
+// interface ExampleInputs {
+//   collaborators: '[{"login":"pavlovcik","id":4975670,"node_id":"MDQ6VXNlcjQ5NzU2NzA=","avatar_url":"https://avatars.githubusercontent.com/u/4975670?v=4","gravatar_id":"","url":"https://api.github.com/users/pavlovcik","html_url":"https://github.com/pavlovcik","followers_url":"https://api.github.com/users/pavlovcik/followers","following_url":"https://api.github.com/users/pavlovcik/following{/other_user}","gists_url":"https://api.github.com/users/pavlovcik/gists{/gist_id}","starred_url":"https://api.github.com/users/pavlovcik/starred{/owner}{/repo}","subscriptions_url":"https://api.github.com/users/pavlovcik/subscriptions","organizations_url":"https://api.github.com/users/pavlovcik/orgs","repos_url":"https://api.github.com/users/pavlovcik/repos","events_url":"https://api.github.com/users/pavlovcik/events{/privacy}","received_events_url":"https://api.github.com/users/pavlovcik/received_events","type":"User","site_admin":false,"permissions":{"admin":true,"maintain":true,"push":true,"triage":true,"pull":true},"role_name":"admin"}]';
+//   eventName: "issues.closed";
+//   issueNumber: "25";
+//   issueOwner: "pavlovcik";
+//   issueRepository: "ubiquibot-sandbox";
+// }
 
 async function run() {
   const { SUPABASE_URL, SUPABASE_KEY, openAi } = checkEnvironmentVariables();
   const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
   const webhookPayload = github.context.payload;
-  const inputs = webhookPayload.inputs as DelegatedComputeInputs;
+  const inputs = webhookPayload.inputs as DelegatedComputeInputs; //as ExampleInputs;
 
   console.trace({ inputs });
 
@@ -50,7 +58,9 @@ async function issueClosedEventHandler(supabaseClient: SupabaseClient, openAi: O
 
   const config = await getConfig(inputs.issueOwner, inputs.issueRepository);
 
-  const collaborators = await Promise.all(inputs.collaborators.map((login) => getUser(login)));
+  const  collaboratorsParsed = JSON.parse(inputs.collaborators);
+
+  const collaborators = await Promise.all(collaboratorsParsed.map((login: GitHubUser["login"]) => getUser(login)));
 
   const result: string = await issueClosed({
     issue,
