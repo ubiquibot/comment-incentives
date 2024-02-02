99002ccb1f5c6bfe3ddb404a71960aa013ca0017
アレクサンダー.eth <gpg@pavlovcik.com>
2024-02-02 19:37:15 +0900

    Revert "feat: update"
    This reverts commit 1d65783bea01c100f355bd6b56056cbf6a5f051e.

--- src/handlers/issue/issue-closed.ts
+++ src/handlers/issue/issue-closed.ts
@@ -26,7 +26,6 @@ export async function issueClosed({
     openAi,
     pullRequestComments,
   });
-  console.trace("sourceScores: ",sourceScores);
   // 2. sum total scores will sum the scores of every contribution, and organize them by contributor
   const contributorTotalScores = sumTotalScores(sourceScores);
   // 3. generate permits will generate a payment for every contributor
