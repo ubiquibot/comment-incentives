539fd4535618eb83ce9a4fd21d87b83609121e69
アレクサンダー.eth <gpg@pavlovcik.com>
2024-02-02 19:38:11 +0900

    Revert "feat: remove axios call"
    This reverts commit a4f7bb240b2aa3ec476de1b8bcc9282e7328bf50.

--- src/helpers/get-linked-issues-and-pull-requests.ts
+++ src/helpers/get-linked-issues-and-pull-requests.ts
@@ -1,8 +1,8 @@
-import { Octokit } from "@octokit/rest";
+import axios from "axios";
+import { HTMLElement, parse } from "node-html-parser";
 // import { Context } from '../types/context';
 
 interface GetLinkedParams {
-  authenticatedOctokit: Octokit,
   owner: string;
   repository: string;
   issue?: number;
@@ -16,77 +16,56 @@ interface GetLinkedResults {
   href: string;
 }
 
-export async function getLinkedIssues({ authenticatedOctokit, owner, repository, pull }: GetLinkedParams) {
-  if(!pull) return null;
-  const { data } = await authenticatedOctokit.pulls.get({
-    owner,
-    repo: repository,
-    pull_number: pull
-  });
+export async function getLinkedIssues({ owner, repository, pull }: GetLinkedParams) {
+  const { data } = await axios.get(`https://github.com/${owner}/${repository}/pull/${pull}`);
+  const dom = parse(data);
+  const devForm = dom.querySelector("[data-target='create-branch.developmentForm']") as HTMLElement;
+  const linkedIssues = devForm.querySelectorAll(".my-1");
 
-  const body = data.body;
-  if(!body) return null;
-
-  const match = body.match(/#(\d+)/);
-  const issueNumber = match ? match[1] : null;
-
-  if (!issueNumber) {
+  if (linkedIssues.length === 0) {
     return null;
   }
 
-  const issue = await authenticatedOctokit.issues.get({
-    owner,
-    repo: repository,
-    issue_number: Number(issueNumber)
-  });
-
-  return issue.data.html_url;
+  const issueUrl = linkedIssues[0].querySelector("a")?.attrs?.href || null;
+  return issueUrl;
 }
 
 export async function getLinkedPullRequests(
   // context: Context,
-  { authenticatedOctokit, owner, repository, issue }: GetLinkedParams
+  { owner, repository, issue }: GetLinkedParams
 ): Promise<GetLinkedResults[]> {
   // const logger = context.logger;
   const collection = [] as GetLinkedResults[];
-  const pulls = await getAllPullRequests(authenticatedOctokit, owner, repository);
-  const currentIssue = await authenticatedOctokit.issues.get({
-    owner,
-    repo: repository,
-    issue_number: issue
-  });
-  for (const pull of pulls) {
-    const linkedIssue = await getLinkedIssues({
-      authenticatedOctokit,
-      owner: owner,
-      repository: repository,
-      pull: pull.number,
-    });
+  const { data } = await axios.get(`https://github.com/${owner}/${repository}/issues/${issue}`);
+  const dom = parse(data);
+  const devForm = dom.querySelector("[data-target='create-branch.developmentForm']") as HTMLElement;
+  const linkedList = devForm.querySelectorAll(".my-1");
+  if (linkedList.length === 0) {
+    // context.logger.info(`No linked pull requests found`);
+    return [];
+  }
 
-    if(linkedIssue === currentIssue.data.html_url) {
-      collection.push({
-        organization: owner,
-        repository,
-        number: pull.number,
-        href: pull.html_url,
-      })
+  for (const linked of linkedList) {
+    const relativeHref = linked.querySelector("a")?.attrs?.href;
+    if (!relativeHref) continue;
+    const parts = relativeHref.split("/");
+
+    // check if array size is at least 4
+    if (parts.length < 4) continue;
+
+    // extract the organization name and repo name from the link:(e.g. "
+    const organization = parts[parts.length - 4];
+    const repository = parts[parts.length - 3];
+    const number = Number(parts[parts.length - 1]);
+    const href = `https://github.com${relativeHref}`;
+
+    if (`${organization}/${repository}` !== `${owner}/${repository}`) {
+      // logger.info("Skipping linked pull request from another repository", href);
+      continue;
     }
+
+    collection.push({ organization, repository, number, href });
   }
 
   return collection;
-}
-
-export async function getAllPullRequests(authenticatedOctokit: Octokit, owner: string, repo: string, state: "open" | "closed" | "all" = "open",) {
-
-  try {
-    const pulls = await authenticatedOctokit.paginate(authenticatedOctokit.rest.pulls.list, {
-      owner,
-      repo,
-      state,
-      per_page: 100,
-    });
-    return pulls;
-  } catch (err: unknown) {
-    return [];
-  }
 }
