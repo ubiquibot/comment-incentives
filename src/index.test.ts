93a5a37ce3116ce50d1187f6c703f552dee37841
アレクサンダー.eth <gpg@pavlovcik.com>
2024-02-17 00:36:04 +0900

    refactor: async await

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,23 +5,30 @@ import { GitHubEvent } from "./types/payload";
 
 const { SUPABASE_URL, SUPABASE_KEY, openAi } = checkEnvironmentVariables();
 
-getAuthenticatedOctokit()
-  .then((authenticatedOctokit) => {
-    issueClosedEventHandler(createClient(SUPABASE_URL, SUPABASE_KEY), openAi, authenticatedOctokit, {
-      eventName: GitHubEvent.ISSUES_CLOSED,
-      issueOwner: "ubiquibot",
-      issueRepository: "production",
-      issueNumber: "84",
-      collaborators: `["pavlovcik"]`,
-      installationId: "37627918", // "ubiquibot-dev" app
-    })
-      .then(() => {
-        console.log("issueClosedEventHandler done");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  })
-  .catch((error) => {
+export async function indexTest() {
+  try {
+    const authenticatedOctokit = await getAuthenticatedOctokit();
+    try {
+      const output = await issueClosedEventHandler(
+        createClient(SUPABASE_URL, SUPABASE_KEY),
+        openAi,
+        authenticatedOctokit,
+        {
+          eventName: GitHubEvent.ISSUES_CLOSED,
+          issueOwner: "ubiquibot",
+          issueRepository: "production",
+          issueNumber: "84",
+          collaborators: `["pavlovcik"]`,
+          installationId: "37627918", // "ubiquibot-dev" app
+        }
+      );
+      console.trace({ output });
+    } catch (error) {
+      console.error(error);
+    }
+  } catch (error) {
     console.error(error);
-  });
+  }
+}
+
+void indexTest();
