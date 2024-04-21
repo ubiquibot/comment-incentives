055a839648b2799aac762bc77c26cf747fff2dc9
Korrrba <gitcoiner@gmail.com>
2024-04-21 22:29:29 +0200

    feat: use ts-retry wrappers in getTokenSymbol

--- src/helpers/contracts.ts
+++ src/helpers/contracts.ts
@@ -1,9 +1,17 @@
 import { ethers } from "ethers";
 import { abi as ERC20ABI } from "@openzeppelin/contracts/build/contracts/ERC20.json";
+import { retryAsync, retryAsyncUntilDefined } from "ts-retry";
+import { JsonRpcProvider } from "@ethersproject/providers";
 
 export async function getTokenSymbol(tokenAddress: string, rpcUrl: string): Promise<string> {
-  const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+  const provider = await retryAsyncUntilDefined<JsonRpcProvider>(
+    async () => new ethers.providers.JsonRpcProvider(rpcUrl),
+    { delay: 1000, maxTry: 5 }
+  );
   const contractInstance = new ethers.Contract(tokenAddress, ERC20ABI, provider);
-  const symbol = await contractInstance.symbol();
+  const symbol = await retryAsync<string>(
+    async () => await contractInstance.symbol(),
+    { delay: 1000, maxTry: 5 }
+  );
   return symbol;
 }
