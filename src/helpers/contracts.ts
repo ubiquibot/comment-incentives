8053df57a996420f52b3ccd1dd9923ddb566a578
Korrrba <gitcoiner@gmail.com>
2024-04-22 18:22:41 +0200

    feat: overload getTokenSymbol to accept rpc url or JsonRpcProvider object

--- src/helpers/contracts.ts
+++ src/helpers/contracts.ts
@@ -3,15 +3,24 @@ import { abi as ERC20ABI } from "@openzeppelin/contracts/build/contracts/ERC20.j
 import { retryAsync, retryAsyncUntilDefined } from "ts-retry";
 import { JsonRpcProvider } from "@ethersproject/providers";
 
-export async function getTokenSymbol(tokenAddress: string, rpcUrl: string): Promise<string> {
-  const provider = await retryAsyncUntilDefined<JsonRpcProvider>(
-    async () => new ethers.providers.JsonRpcProvider(rpcUrl),
-    { delay: 1000, maxTry: 5 }
-  );
+
+export async function getTokenSymbol(tokenAddress: string, rpcUrl: string): Promise<string>;
+export async function getTokenSymbol(tokenAddress: string, provider: JsonRpcProvider): Promise<string>;
+export async function getTokenSymbol(tokenAddress: string, rpc: string|JsonRpcProvider): Promise<string> {
+  let provider: JsonRpcProvider;
+
+  if (typeof rpc === "string") {
+      provider = await retryAsyncUntilDefined<JsonRpcProvider>(
+        async () => new ethers.providers.JsonRpcProvider(rpc),
+        { maxTry: 5 }
+      );
+  } else provider = rpc;
+
   const contractInstance = new ethers.Contract(tokenAddress, ERC20ABI, provider);
   const symbol = await retryAsync<string>(
     async () => await contractInstance.symbol(),
-    { delay: 1000, maxTry: 5 }
+    { maxTry: 5 }
   );
   return symbol;
 }
+
